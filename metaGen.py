8ffc971fa536cbee86ed9a340a00bca15643d2b8
PtrMan <robertw89@googlemail.com>
2019-03-18 08:28:59 +0100

    Refactor: metaGen

--- metaGen.py
+++ metaGen.py
@@ -41,12 +41,6 @@ derivationFunctionsSrc = ""
 
 # code generator : emit code
 def genEmit(premiseA, premiseB, preconditions, conclusion, truthTuple, desire):
-    # unpack truthTuple into truth and intervalProjection
-    (truth, intervalProjection) = truthTuple
-
-    (premiseASubj, premiseACopula, premiseAPred) = premiseA
-    (premiseBSubj, premiseBCopula, premiseBPred) = premiseB
-    (conclusionSubj, conclusionCopula, conclusionPred) = conclusion
 
     def escape(str_):
         return str_.replace("\\", "\\\\")
@@ -60,6 +54,16 @@ def genEmit(premiseA, premiseB, preconditions, conclusion, truthTuple, desire):
             else:
                 asStringList.append('"' + str(iPathElement) + '"')
         return "[" + ",".join(asStringList) + "]"
+
+
+
+    # unpack truthTuple into truth and intervalProjection
+    (truth, intervalProjection) = truthTuple
+
+    (premiseASubj, premiseACopula, premiseAPred) = premiseA
+    (premiseBSubj, premiseBCopula, premiseBPred) = premiseB
+    (conclusionSubj, conclusionCopula, conclusionPred) = conclusion
+
 
 
     # need to figure out which terms are the same on both sides
@@ -483,39 +487,39 @@ for [copAsym,copSym,[ConjCops,DisjCop,MinusCops]] in CopulaTypes:
     # TODO< implement inference generation function to generate code which accepts only one argument >
     #print "(A "+copAsym+" B)\t\t\t\t\t|-\t(B "+ival(copAsym,"-t")+" A)\t\t(Truth:Conversion)"
     
-    if True:
+    if False:
         #print "(A "+copAsym+" B),\t(B "+copAsymZ+" C)\t\t\t|-\t(A "+ival(copAsym,"t+z")+" C)\t\t(Truth:deduction"+OmitForHOL(", Desire:Strong")+")"
         gen(("A",copAsym,"B"), ("B",copAsymZ,"C"),   [] ,("A",ival(copAsym,"t+z"),"C"),    ("deduction", ""), OmitForHOL("strong"))
     
     copAsymHasTimeOffset = "/" in str(copAsym) or "\\" in str(copAsym)
     IntervalProjection = "IntervalProjection(t,z)" if copAsymHasTimeOffset else ""
     
-    if True: # block
+    if False: # block
         gen(("A", copAsym, "B"),   ("C", copAsymZ, "B"),   [], ("A", ival(copAsym, "t-z"), "C"),   ("induction", IntervalProjection), OmitForHOL("weak"))
     
-    if True:
+    if False:
         gen(("A", copAsym, "B"),   ("A", copAsymZ, "C"),   [], ("B", ival(copAsym, "t-z"), "C"),   ("abduction", IntervalProjection), OmitForHOL("strong"))
 
     if copSym != None:
         copSymZ = ival(copSym,"z")
         
-        if True:
+        if False:
             #print "(A "+copSym+" B),\t(B "+copSymZ+" C)\t\t\t|-\t(A "+ival(copSym,"t+z")+" C)\t\t(Truth:resemblance"+OmitForHOL(", Desire:Strong")+")"
             gen(("A",copSym,"B"),("B",copSymZ,"C"),  [], ("A",ival(copSym,"t+z"),"C"),  ("resemblance", ""), OmitForHOL("strong"))
 
-        if True:
+        if False:
             #print "(A "+copAsym+" B),\t(C "+copSymZ+" B)\t\t\t|-\t(A "+copAsym+" C)\t\t(Truth:analogy"+IntervalProjection+OmitForHOL(", Desire:Strong")+")"
             gen(("A",copAsym,"B"),("C",copSymZ,"B"),   [], ("A",copAsym,"C"),   ("analogy", IntervalProjection), OmitForHOL("strong"))
 
-        if True:
+        if False:
             #print "(A "+copAsym+" B),\t(C "+copSymZ+" A)\t\t\t|-\t(C "+ival(copSym,"t+z")+" B)\t\t(Truth:analogy"+OmitForHOL(", Desire:Strong")+")"
             gen(("A",copAsym,"B"),("C",copSymZ,"A"),   [], ("C",ival(copSym,"t+z"),"B"),  ("analogy", ""), OmitForHOL("strong"))
         
-        if True:
+        if False:
             #print "(A "+copAsym+" B),\t(C "+copSymZ+" B)\t\t\t|-\t(A "+copSym+" C)\t\t(Truth:comparison"+IntervalProjection+OmitForHOL(", Desire:Weak")+")"
             gen(("A", copAsym, "B"),  ("C", copSymZ, "B"),   [],("A",copSym,"C"), ("comparison", IntervalProjection), OmitForHOL("weak"))
 
-        if True:
+        if False:
             #print "(A "+copAsym+" B),\t(A "+copSymZ+" C)\t\t\t|-\t(C "+copSym+" B)\t\t(Truth:comparison"+IntervalProjection+OmitForHOL(", Desire:Weak")+")"
             gen(("A", copAsym, "B"),  ("A",copSymZ,"C"),    [],("C",copSym,"B"), ("comparison", IntervalProjection), OmitForHOL("weak"))
     
@@ -547,7 +551,7 @@ for [copAsym,copSym,[ConjCops,DisjCop,MinusCops]] in CopulaTypes:
 
         copZ = ival(cop,"z")
         if MinusCops != None:
-            if True:
+            if False:
                 gen(("A",cop,"B"),("C",copZ,"B"),   [], ((MinusCops[1],"A","C"),cop,"B"),    ("difference", ""), "")
                 gen(("A",cop,"B"),("A",copZ,"C"),   [], ("B",cop,(MinusCops[0],"A","C")),    ("difference", ""), "")
                 gen(("S",cop,"M"),((MinusCops[1],"S","P"),copZ,"B"),   [],("P",cop,"M"),   ("decomposePNP", ""), "")
@@ -573,18 +577,18 @@ for [copAsym,copSym,[ConjCops,DisjCop,MinusCops]] in CopulaTypes:
 
                     if not isinstance(cop, CWT): # is disabled for temporal inference until we can traverse & build these complicated binary terms
 
-                        if True:
+                        if False:
                             #print "(A "+cop+" B),\t(C "+copZ+" B)\t\t\t|-\t((" + junc + " A C) "+ cop + " B) \t" + TruthSet1 + IntervalProjection+")"
                             gen(("A",cop,"B"),("C",copZ,"B"),   [],((junc,"A", "C"), cop, "B"),  (TruthSet1, IntervalProjection), "")
 
-                        if True:
+                        if False:
                             #print "(A "+cop+" B),\t(A "+copZ+" C)\t\t\t|-\t(A "+ cop + " (" + junc + " B C)) \t"  + TruthSet2 + IntervalProjection+")"
                             gen(("A",cop,"B"),("A",copZ,"C"),   [],("A",cop,(junc,"B", "C")),  (TruthSet2, IntervalProjection), "")
 
-                        if True:
+                        if False:
                             gen(("S",cop,"M"),((junc,"S", "L"),copZ,"M"),    [],("L",cop,"M"),   (TruthDecomp1, IntervalProjection), "")
                         
-                        if True:
+                        if False:
                             gen(("M",cop,"S"),("M",copZ,(junc,"S","L")),     [],("M",cop,"L"),   (TruthDecomp2, IntervalProjection), "")
 
 print "  return rootTries;"
