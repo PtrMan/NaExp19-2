0eafbe2576cf419e5dca9d685a7587835b6568a1
PtrMan <robertw89@googlemail.com>
2019-03-21 18:53:20 +0100

    Refactor: use of lambdas

--- TruthValue.d
+++ TruthValue.d
@@ -32,12 +32,12 @@ class TruthValue {
 
 		if (function_ == "analogy") {
         	double f = and(f1, f2);
-        	double c = and(c1, c2, f2);
+        	double c = and3(c1, c2, f2);
 			return new shared TruthValue(cast(float)f, c);
 		}
 		else if(function_ == "resemblance") {
 			double f = and(f1, f2);
-        	double c = and(c1, c2, or(f1, f2));
+        	double c = and3(c1, c2, or(f1, f2));
 			return new shared TruthValue(cast(float)f, c);
 		}
 		else if(function_ == "induction") {
@@ -48,18 +48,18 @@ class TruthValue {
 		}
 		else if(function_ == "deduction") {
 			double f = and(f1, f2);
-        	double c = and(c1, c2, f);
+        	double c = and3(c1, c2, f);
 			return new shared TruthValue(cast(float)f, c);
 		}
 		else if(function_ == "resemblance") {
 			double f = and(f1, f2);
-        	double c = and(c1, c2, or(f1, f2));
+        	double c = and3(c1, c2, or(f1, f2));
 			return new shared TruthValue(cast(float)f, c);
 		}
 		else if(function_ == "comparison") {
 			double f0 = or(f1, f2);
         	double f = (f0 == 0.0) ? 0.0 : (and(f1, f2) / f0);
-        	double w = and(f0, c1, c2);
+        	double w = and3(f0, c1, c2);
         	double c = w2c(w, horizon);
 			return new shared TruthValue(cast(float)f, c);
 		}
@@ -85,7 +85,7 @@ class TruthValue {
 			// see https://github.com/jarradh/narjure/blob/master/src/nal/truth_value.clj#L94-L99
 			double f2n = not(f2),
 			       f = and(f1, f2n),
-			       c = and(f, c1, c2);
+			       c = and3(f, c1, c2);
 			return new shared TruthValue(f, c);
 		}
 		else if("decomposeNNN") {
@@ -94,14 +94,14 @@ class TruthValue {
 			       f2n = not(f2),
 			       fn = and(f1n, f2n),
 			       f = not(fn),
-			       c = and(fn, c1, c2);
+			       c = and3(fn, c1, c2);
 			return new shared TruthValue(f, c);
 		}
 		else if("decomposeNPP") {
 			// see https://github.com/jarradh/narjure/blob/master/src/nal/truth_value.clj#L87-L92
 			double f1n = not(f1),
 			       f = and(f1n, f2),
-			       c = and(f, c1, c2);
+			       c = and3(f, c1, c2);
 			return new shared TruthValue(f, c);
 		}
 		else if("decomposePNN") {
@@ -109,7 +109,7 @@ class TruthValue {
 			double f2n = not(f2),
 			       fn = and(f1, f2n),
 			       f = not(fn),
-			       c = and(fn, c1, c2);
+			       c = and3(fn, c1, c2);
 			return new shared TruthValue(f, c);
 		}
 		// TODO< implement other truth functions >
@@ -123,7 +123,7 @@ class TruthValue {
 		double f2 = b.freq;
 		double c2 = b.conf;
 
-		double w = and(f2, c1, c2);
+		double w = and3(f2, c1, c2);
         double c = w2c(w, horizon);
 		return new shared TruthValue(cast(float)f1, c);
 	}
@@ -135,18 +135,10 @@ class TruthValue {
 		return horizon * c / (1.0 - c);
 	}
 
-	private static double or(double a, double b) {
-		return (1.0-a)*(1.0-b);
-	}
-	private static double and(double a, double b) {
-		return a*b;
-	}
-	private static double and(double a, double b, double c) {
-		return a*b*c;
-	}
-	private static double not(double v) {
-		return 1.0-v;
-	}
+	static auto and3 = (double a, double b, double c) => a*b*c;
+	static auto or = (double a, double b) => (1.0-a)*(1.0-b);
+	static auto and = (double a, double b) => a*b;
+	static auto not (double v) => 1.0-v;
 }
 
 double calcExp(shared TruthValue tv) {
